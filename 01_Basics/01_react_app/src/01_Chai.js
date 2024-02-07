a4bfc8cbdfca0907e17c151f81282f450e924b87
tajansari612 <tajansari612@gmail.com>
2024-02-07 14:33:54 +0530

    new file created

---
+++ 01_Basics/01_react_app/src/01_Chai.js
@@ -0,0 +1,11 @@
+
+function Chai() {
+    return (
+        <>
+            <h1>Chai aur react</h1>
+            <h2>This is a heading tag h2</h2>
+            <p>This is a paragraph</p>
+        </>
+    )
+}
+export default Chai;
---
+++ 01_Basics/01_vitereact_app/src/01_Chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h1>Chai aur React with vite</h1>
+    )
+}
+
+export default Chai;
