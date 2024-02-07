cd7beb245280ee4c469cbe0d6ab65fddf63d6ebf
tajansari612 <tajansari612@gmail.com>
2024-02-07 11:18:36 +0530

    new file created

---
+++ 01_Basics/01_react_app/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>New React JS Project</h1>
+  );
+}
+
+export default App;
---
+++ 01_Basics/01_vitereact_app/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+  
+  return (
+   <h1>New React Project with vite</h1>
+  )
+}
+
+export default App
